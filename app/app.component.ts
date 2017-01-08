648b1f0bd7484886711e77124ecc7475e4636b3a
Pervaze Ahanger <iampervaze@gmail.com>
2017-01-08 13:38:04 +0530

    First

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    template: "<h1>{{welcomeMessage}}</h1>"
+})
+
+export class AppComponent {
+    welcomeMessage: string = "Welcome to your first component!";
+}
