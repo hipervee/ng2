a60628344e170c26277ab5a143d6f460ce946398
Pervaze Ahanger <iampervaze@gmail.com>
2017-01-08 16:21:34 +0530

    Create pipe Filter to Filter Movies

---
+++ app/movies/movie-list.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from "@angular/core";
+import { IMovie} from "./movie";
+import { MovieFilterPipe } from "./movie-filter.pipe";
+
+@Component({
+    selector: "mm-movies",
+    templateUrl: "app/movies/movie-list.component.html",
+    styleUrls: ["app/movies/movie-list.component.css"],
+    pipes: [MovieFilterPipe]
+})
+
+export class MovieListComponent implements OnInit {
+    title: string = 'Movie List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    seePoster: boolean =  false;
+    movies: IMovie[] = [
+        {
+            "movieId": 2,
+            "movieName": "Titanic!",
+            "movieStar": "DiCaprio",
+            "releaseDate": "3/13/2016",
+            "price": 8.00,
+            "starRating": 4.5,
+            "imageUrl": "http://ia.media-imdb.com/images/M/MV5BMzg1MDA0MTU2Nl5BMl5BanBnXkFtZTcwMTMzMjkxNw@@._V1_.jpg"
+        },
+        {
+            "movieId": 3,
+            "movieName": "Jaws!",
+            "movieStar": "Shaw",
+            "releaseDate": "4/13/2016",
+            "price": 6.00,
+            "starRating": 4.8,
+            "imageUrl": "http://ia.media-imdb.com/images/M/MV5BNDcxODkyMjY4MF5BMl5BanBnXkFtZTgwOTk5NTc5MDE@._V1_SY1000_CR0,0,666,1000_AL_.jpg"
+        }
+    ];
+    toggleImage(): void {
+        this.seePoster = !this.seePoster;
+    };
+
+    ngOnInit(): void {
+        console.log("Init Called");
+    }
+}
---
+++ app/movies/movie.ts
@@ -0,0 +1,9 @@
+export interface IMovie {
+    movieId: number;
+    movieName: string;
+    movieStar: string;
+    releaseDate: string;
+    price: number;
+    starRating: number;
+    imageUrl: string;
+}
